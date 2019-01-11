3edb5546a1d9ca803705881ea0d713c080319ee8
Suveen Vundavalli <Suveen.Kumar.Vundavalli@clxcommunications.com>
2019-01-11 17:43:07 +0100

    Initial commit

---
+++ src/test/java/com/clx/apps/tt/TtApplicationTests.java
@@ -0,0 +1,18 @@
+package com.clx.apps.tt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		// Test cases will be added later
+	}
+
+}
+
